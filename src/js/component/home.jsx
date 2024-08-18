e0e45ef65161821d062c9056cf5361ed69a3f183
Frcru <91342407+Frcru@users.noreply.github.com>
2024-08-18 04:03:32 +0000

    play and reset works buut for reset we need to click two times

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,51 +4,82 @@ import SecondsCounter from "./SecondsCounter.jsx";
 const Home = () => {
 	const [counter, setCounter] = useState(0);
 	const [divValues, setDivValues] = useState([]);
-	let interval;
+	const [resume, setResume] = useState(true);
+	const [reset, setReset] = useState(false);
+	const [intervalID, setIntervalID] = useState();
 	let b = [...divValues]
-	console.log(b)
 	//for digits of secondscounter
 	useEffect(() => {
-		let n = 0;
-		interval = setInterval(() => {
-			let i = 0;
-			let aux = n;
-			while (aux > 0 && i < 7) {
-				b[i] = aux % 10;
-				setDivValues([...b]);
-				aux = Math.floor(aux / 10);
-				i++;
-			}
-			n++
-		}, 1000)
-	}, [])
+		console.log(resume);
+		clearInterval(intervalID)
+		let n;
+		n = counter
+		if (resume === true) {
+			const newInterval = setInterval(() => {
+				let i = 0;
+				let aux = n;
+				while (aux > 0 && i < 7) {
+					b[i] = aux % 10;
+					setDivValues([...b]);
+					aux = Math.floor(aux / 10);
+					i++;
+				}
+				n++
+				setCounter(n)
+			}, 1000)
+			setIntervalID(newInterval)
+		}
+	}, [resume])
+	useEffect(() => {
+		let n
+		n = 0;
+		if (reset === true) {
+			const newInterval = setInterval(() => {
+				let i = 0;
+				let aux = n;
+				while (aux > 0 && i < 7) {
+					b[i] = aux % 10;
+					setDivValues([...b]);
+					aux = Math.floor(aux / 10);
+					i++;
+				}
+				n++
+				setCounter(0)
+			}, 1000)
+			setIntervalID(newInterval)
+			console.log(intervalID)
+		}
+	}, [reset])
 	//trying to stop first counter but doesn't work
-	function stopHandler() {
-		return clearInterval(interval)
+	function onStopHandler() {
+		setResume(false)
+		clearInterval(intervalID)
 	}
 	//trying to find if input !== 0 so it count until that time but doesn't work
-	function onPlayHandler() {
-		setDivValues(divValues, [])
-		b = [...divValues];
-		let a = 0
-		stopHandler(interval)
-		let interval = setInterval(() => {
-			let i = 0;
-			let aux = a;
-			let stop = counter;
-			while (stop > 0 && aux > 0 && i < 7) {
-				divValues[i] = aux % 10;
-				aux = Math.floor(aux / 10);
-				i++;
-			}
-			a++
-			stop--;
-			if (stop === 0) {
-				alert("se cumplio el tiempo")
-				stopHandler()
-			}
-		}, 1000)
+	function onResetHandler() {
+		if (reset) {
+			clearInterval(intervalID)
+			let aux = [...divValues]
+			aux = [0, 0, 0, 0, 0, 0]
+			setDivValues(aux)
+			setReset(false)
+		}
+		else {
+			clearInterval(intervalID)
+			setReset(true)
+		}
+
 	}
+	function onResumeHandler() {
+		if (resume) {
+			setResume(false)
+			clearInterval(intervalID)
+		} else {
+			setResume(true)
+			console.log(2)
+		}
+	}
+
 	return (
 		<div>
 			<div className="container-fluid d-flex justify-content-between bg-dark align-items-center position-relative" style={{ height: `120px` }}>
@@ -63,15 +94,15 @@ const Home = () => {
 					<SecondsCounter count={divValues[0] === undefined ? 0 : divValues[0]} />
 				</div>
 			</div>
-			<div classname="container-fluid d-flex justify-content-center">
+			<div className="container-fluid d-flex flex-column justify-content-center">
 				<div className="container d-flex flex-column justify-content-center" style={{ maxWidth: "250px" }}>
 					<span className="input-group-text" id="basic-addon1">Set your time</span>
 					<input min="10" type="number" onChange={(e) => setCounter(e.target.value)} className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
 				</div>
 
 				<div className="container-fluid d-flex justify-content-center align-items-center">
-					<button className="btn btn-success me-3" onClick={onPlayHandler}><i className="fa-solid fa-play"></i></button>
-					<button className="btn btn-success" onClick={stopHandler}><i className="fa-solid fa-stop"></i></button>
+					<button className="btn btn-success me-3" onClick={() => onResumeHandler()}><i className="fa-solid fa-play"></i></button>
+					<button className="btn btn-success" onClick={() => onResetHandler()}><i className="fa-solid fa-stop"></i></button>
 				</div>
 			</div>
 		</div>
